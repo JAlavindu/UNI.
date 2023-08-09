1c63a74d3345f00f996a4edbca8ff8735ce6c68d
Theshawa <mrclocktd@gmail.com>
2023-08-09 13:44:34 +0530

    project init

---
+++ src/pages/Feed.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Feed: FC = () => {
+  return (
+    <>
+      <div>Home Feed</div>
+    </>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#ffffff",
+      "light-blue": "#DFEAF7",
+      "gray-F3F2EF": "#F3F2EF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
